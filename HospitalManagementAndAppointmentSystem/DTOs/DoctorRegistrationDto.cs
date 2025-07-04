4e1ecd333627be5ed1312a54261f711be6e7f010
Kiruthiga LNU <2417909@cognizant.com>
2025-07-04 16:53:46 +0530

    User Registration and Login DTO's created

---
+++ HospitalManagementAndAppointmentSystem/DTOs/DoctorRegistrationDto.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Domain.Models.Enum;
+
+namespace HospitalManagementAndAppointmentSystem.DTOs
+{
+    public class DoctorRegistrationDto 
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(20,MinimumLength =8)]
+        public string Password { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string EmergencyContactName { get; set; }
+        [Required]
+        public Relationship EmergencyContactRelationship { get; set; }
+        [Required]
+        public string EmergencyContactPhoneNumber { get; set; }
+        [Required]
+        public string Specialization { get; set; }
+        [Required]
+        public string Qualification { get; set; }
+        [Required]
+        public int ExperienceYears { get; set; }
+    }
+}
